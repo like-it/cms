2e7aa03409e6f341a15d563ebff6903f8e7320c5
Remco van der Velde <remco.pc@outlook.com>
2021-10-06 23:36:28 +0200

    install.start

--- src/Core/Install/Plugin/Function_install_start.php
+++ src/Core/Install/Plugin/Function_install_start.php
@@ -3,7 +3,7 @@
 use R3m\Io\Module\Data;
 use R3m\Io\Module\Parse;
 
-function function_install(Parse $parse, Data $data){
+function function_install_start(Parse $parse, Data $data){
     $object = $parse->object();
     return \LikeIt\Cms\Core\Install\Service\Install::start($object);
 
