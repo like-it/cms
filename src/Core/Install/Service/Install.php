d110664f123877d61506f11091f5cdcf366d9fcf
Remco van der Velde <remco.pc@outlook.com>
2021-10-06 23:45:21 +0200

    validator

--- src/Core/Install/Service/Install.php
+++ src/Core/Install/Service/Install.php
@@ -5,10 +5,37 @@ use JetBrains\PhpStorm\NoReturn;
 use R3m\Io\App;
 
 use Exception;
+use R3m\Io\Module\Validate;
 
 class Install {
 
     public static function start(App $object){
+        $email = $object->request('email');
+        $password = $object->request('password');
+        $password2 = $object->request('password2');
+        $domain = $object->request('domain');
+
+        $url = $object->data('controller.dir.data') . File::basename(__CLASS__) . $object->config('extension.json');
+        d($url);
+        $object->request('node.type', 'user');
+        $validate_1 = Install::validate($object, $url);
+
+
         dd($object->request());
+    }
+
+    public static function validate(App $object, $url){
+        $data = $object->data(sha1($url));
+        if($data === null){
+            $data = $object->parse_read($url, sha1($url));
+        }
+        if($data){
+            $validate = $data->data($object->request('node.type') . '.validate');
+            try {
+                return Validate::validate($object, $validate);
+            } catch (Exception $e) {
+                echo $e;
+            }
+        }
     }
 }
