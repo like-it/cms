6f90dd581bae85373f9659d9542dcd94698b6d93
Remco van der Velde <remco.pc@outlook.com>
2021-10-20 22:47:56 +0200

    Update User.php

--- src/Core/Install/Data/Core/Service/User.php
+++ src/Core/Install/Data/Core/Service/User.php
@@ -13,24 +13,30 @@ use R3m\Io\Exception\UrlEmptyException;
 use R3m\Io\Exception\UrlNotExistException;
 
 class User extends Main {
+    const PASSWORD_ALGO = PASSWORD_BCRYPT;
+    const PASSWORD_COST = 13;
 
     public static function create(App $object){
-
-        $url = $object->config('host.dir.root') .
-            'Node' .
-            $object->config('ds') .
-            'Validator' .
-            $object->config('ds') .
-            File::basename(__CLASS__) .
-            $object->config('extension.json');
-
         $object->request('uuid', Core::uuid());
         $object->request('node.type', 'user');
-        $validate = Main::validate($object, $url);
+        $validate = Main::validate($object, User::getValidatorUrl($object));
         if($validate){
             if($validate->success === true){
-                //we can create the user
-                dd('create User');
+                $data = $object->data_read(User::getDataUrl($object));
+                if(!$data){
+                    $data = new Data();
+                }
+                $uuid = $object->request('uuid');
+                $password = password_hash($object->request('password'),User::PASSWORD_ALGO, ['cost' => User::PASSWORD_COST]);
+                $email = $object->request('email');
+                $data->set($uuid . '.uuid', $uuid);
+                $data->set($uuid . '.email', $email);
+                $data->set($uuid . '.password', $password);
+                $data->set($uuid . '.isActive', false);
+                $data->write(User::getDataUrl($object));
+                //store activation code in userParameter with activation expiration date...
+                //send activation email to user...
+                return new Response($data->get($uuid), Response::TYPE_JSON);
             } else {
                 return new Response($validate->test, Response::TYPE_JSON, 400);
             }
@@ -55,6 +61,28 @@ class User extends Main {
 
     public static function list(App $object){
         dd('list Users');
+    }
+
+    private static function getValidatorUrl(App $object){
+        $url = $object->config('host.dir.root') .
+            'Node' .
+            $object->config('ds') .
+            'Validator' .
+            $object->config('ds') .
+            File::basename(__CLASS__) .
+            $object->config('extension.json');
+        return $url;
+    }
+
+    private static function getDataUrl(App $object){
+        $url = $object->config('host.dir.root') .
+            'Node' .
+            $object->config('ds') .
+            'List' .
+            $object->config('ds') .
+            File::basename(__CLASS__) .
+            $object->config('extension.json');
+        return $url;
     }
 
 }
