9f0f5de0362f99147fe4f3f8fc488932568a4534
Remco van der Velde <remco.pc@outlook.com>
2021-11-07 00:02:48 +0100

    upload

---
+++ src/Core/Install/Data/Cms/Public/Module/Upload.js
@@ -0,0 +1,52 @@
+import { user } from "/Module/User.js";
+
+let upload = {};
+
+upload.init = () => {
+    const body = select('body');
+    if(!body){
+        return;
+    }
+    let upload = body.select('.upload');
+    let token = user.token();
+    if(!upload) {
+        upload = create('div', 'dropzone upload');
+        upload.attribute('id', 'upload');
+        upload.data('url', 'core.funda.local:2610/Import/');
+        upload.data('upload-max-filesize', '1024 M');
+        body.appendChild(upload);
+        if(token){
+            let drop = new Dropzone(
+                '#' + upload.attribute('id'), {
+                    url: upload.data('url'),
+                    maxFilesize: upload.data('upload-max-filesize'),
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }
+            );
+            drop.on("sending", function (file, xhr, formData) {
+                //xhr.setRequestHeader('Authorization', 'Bearer ' + user.token());
+                /*
+                exception.authorization(() => {
+
+                });
+                 */
+            });
+            drop.on("complete", function (file) {
+                console.log('start unpacking...');
+                console.log(file);
+                /*
+                const refresh = section.select('.refresh');
+                if (!refresh) {
+                    return;
+                }
+                refresh.trigger('click');
+                 */
+            });
+        }
+
+    }
+}
+
+export { upload }
---
+++ src/Core/Install/Data/Cms/Public/Module/User.js
@@ -0,0 +1,23 @@
+let user = {};
+
+user.get = () => {
+    return _('user').collection();
+}
+
+user.set = (data) => {
+    _('user').collection('user', data);
+}
+
+user.data = (attribute, value) => {
+    return _('user').collection(attribute, value);
+}
+
+user.refreshToken = () => {
+    return _('user').collection('user.refresh.token');
+}
+
+user.token = () => {
+    return _('user').collection('user.token');
+}
+
+export { user }
