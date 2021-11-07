828a5293dc5127dc33e9f01c0e649c73835a9f7f
Remco van der Velde <remco.pc@outlook.com>
2021-11-07 22:50:59 +0100

    Update Download.js

--- src/Core/Install/Data/Cms/Public/Module/Export/Download.js
+++ src/Core/Install/Data/Cms/Public/Module/Export/Download.js
@@ -3,7 +3,7 @@ import { user } from "/Module/User.js";
 let download = {};
 
 download.init = () => {
-    request('http://core.funda.local/Export/');
+    request('http://core.funda.local:2610/Export/');
 }
 
 export { download }
