b798c952d1cb9eba6bdd45b8118dbc657fb027ed
Remco van der Velde <remco.pc@outlook.com>
2021-11-07 20:12:51 +0100

    Update Import.php

--- src/Core/Install/Data/Core/Service/Import.php
+++ src/Core/Install/Data/Core/Service/Import.php
@@ -2,12 +2,22 @@
 namespace Host\Subdomain\Host\Extension\Service;
 
 use R3m\Io\App;
+use R3m\Io\Module\File;
 use R3m\Io\Module\Response;
 
+
 class Import extends Main {
-    
+
     public static function import(App $object): Response
     {
+        $upload = $object->upload();
+        foreach($upload->data() as $file){
+            $file->extension = File::extension($file->name);
+            dd($file);
+        }
+
+
+
         d($object->upload());
         dd($object->request());
     }
