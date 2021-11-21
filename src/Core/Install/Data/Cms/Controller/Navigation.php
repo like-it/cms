4240298db68ad9059eb38879e3aea00c302a9547
Remco van der Velde <remco.pc@outlook.com>
2021-11-21 22:13:37 +0100

    Create Navigation.php

---
+++ src/Core/Install/Data/Cms/Controller/Navigation.php
@@ -0,0 +1,24 @@
+<?php
+namespace Host\Subdomain\Host\Extension\Controller;
+
+use R3m\Io\App;
+use R3m\Io\Module\View;
+
+use Exception;
+use R3m\Io\Exception\LocateException;
+use R3m\Io\Exception\UrlEmptyException;
+use R3m\Io\Exception\UrlNotExistException;
+
+class Navigation extends View {
+    const DIR = __DIR__ . DIRECTORY_SEPARATOR;    
+
+    public static function main(App $object){
+        $name = Navigation::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Navigation::locate($object, $name);
+            return Navigation::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+}
