418e1ea5f8953fa5e21198f80bf9ec1843488f99
Remco van der Velde <remco.pc@outlook.com>
2021-11-22 00:15:09 +0100

    Update Navigation.js

--- src/Core/Install/Data/Cms/View/Home/Module/Navigation.js
+++ src/Core/Install/Data/Cms/View/Home/Module/Navigation.js
@@ -13,8 +13,8 @@ ready(() => {
         header('authorization', 'bearer ' + user.token());
         request(active.data('url'), null, (url, response) => {
             if(active.data('frontend-url')){
-                response?.method = null ? active.data('method') : response?.method;
-                response?.target = null ? active.data('target') : response?.target;
+                response?.method !== null ? response.method : active.data('method');
+                response?.target !== null ? response.target : active.data('target') : ;
                 request(active.data('frontend-url'), response);
             }
         });
