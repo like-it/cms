f64b37ec044f667d8146bf0f60131427e75a9e67
Remco van der Velde <remco.pc@outlook.com>
2021-11-22 15:15:19 +0100

    Update Navigation.js

--- src/Core/Install/Data/Cms/View/Navigation/Module/Navigation.js
+++ src/Core/Install/Data/Cms/View/Navigation/Module/Navigation.js
@@ -1,15 +1,30 @@
 {R3M}
+import user from "/Module/User.js";
 import { getSectionByName} from "/Module/Section.js";
 
 ready(() => {
     const section = getSectionByName("{{$controller.name}}");
-    if(!section){
+    if (!section) {
         console.warn('Cannot find section...');
         return;
     }
     const active = section.select('.active');
-    if(active){
+    if (active) {
         active.request();
+    }
+    const list = section.select('.nav-link');
+    let index;
+    for (index = 0; index < list.length; index++) {
+        let node = list[index];
+        node.on('click', (event) => {
+            header('authorization', 'bearer ' + user.token());
+            request(node.data('url'), null, (url, response) => {
+                if(node.data('frontend-url')){
+                    request(node.data('frontend-url'), response);
+                }
+
+            });
+        });
     }
 
 });
