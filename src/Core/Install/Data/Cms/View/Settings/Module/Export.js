ee336e6813806f461fa630871a9fc0f2351501d8
Remco van der Velde <remco.pc@outlook.com>
2021-11-24 22:40:30 +0100

    export click

--- src/Core/Install/Data/Cms/View/Settings/Module/Export.js
+++ src/Core/Install/Data/Cms/View/Settings/Module/Export.js
@@ -6,44 +6,58 @@ ready(() => {
 
     const section = getSectionByName('main-content');
     if(!section){
+
         return;
     }
+
     const list = section.select('.nav-link');
     let index;
     for(index=0; index < list.length; index++){
+
         let node = list[index];
         node.on('click', (event) => {
+
             if(node.data('has', 'url')){
+
                 list.removeClass('active');
                 node.addClass('active');
                 header('authorization', 'Bearer ' + user.token());
                 request(node.data('url'), null, (url, response) => {
+
                     if(node.data('has', 'frontend-url')){
+
                         request(node.data('frontend-url'), response);
                     }
+
                 });
             }
+
             else if(node.data('has', 'frontend-url')){
+
                 request(node.data('frontend-url'));
             }
+
         });
     }
+    
+    const link = section.select('#export');
+    if(link){
 
+        link.on('click', (event) => {
 
+            if(user.token()){
 
+                download.init({
 
+                    url: "{{server.url('core')}}Export/",
+                    filename: "funda-{{config('version')}}.zip",
+                    token: user.token(),
+                });
+            } else {
 
-    if(user.token()){
-
-        download.init({
-
-            url: "{{server.url('core')}}Export/",
-            filename: "funda-{{config('version')}}.zip",
-            token: user.token(),
+                redirect("{{route.get(route.prefix() + '-user-Login')}}");
+            }
         });
-    } else {
-
-        redirect("{{route.get(route.prefix() + '-user-Login')}}");
     }
 
 });
--- src/Core/Install/Data/Cms/View/Settings/Section/Export/Export/Body.tpl
+++ src/Core/Install/Data/Cms/View/Settings/Section/Export/Export/Body.tpl
@@ -1,5 +1,5 @@
 {R3M}
 <div class="card-body h-100">
     <h5 class="card-title">Export</h5>
-    <p class="card-text">Export entire project as zip file.</p>
+    <p class="card-text">Export the entire project as zip file <a id="export">here</a>.</p>
 </div>
