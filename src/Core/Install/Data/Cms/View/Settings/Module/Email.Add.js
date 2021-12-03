adb31f2e7e9822108aab29e47f252d49d43fec69
Remco van der Velde <remco.pc@outlook.com>
2021-12-03 17:58:50 +0100

    Update Email.Add.js

--- src/Core/Install/Data/Cms/View/Settings/Module/Email.Add.js
+++ src/Core/Install/Data/Cms/View/Settings/Module/Email.Add.js
@@ -16,4 +16,24 @@ ready(() => {
             });
         });
     }
+    const list = section.select('.nav-link');
+    let index;
+    for(index=0; index < list.length; index++){
+        let node = list[index];
+        node.on('click', (event) => {
+            if(node.data('has', 'url')){
+                list.removeClass('active');
+                node.addClass('active');
+                header('authorization', 'Bearer ' + user.token());
+                request(node.data('url'), null, (url, response) => {
+                    if(node.data('has', 'frontend-url')){
+                        request(node.data('frontend-url'), response);
+                    }
+                });
+            }
+            else if(node.data('has', 'frontend-url')){
+                request(node.data('frontend-url'));
+            }
+        });
+    }
 });
