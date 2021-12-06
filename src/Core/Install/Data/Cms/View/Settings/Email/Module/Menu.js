196d92f2c14681c4d9e2c1aaa0edc4502ae21641
Remco van der Velde <remco.pc@outlook.com>
2021-12-06 01:15:24 +0100

    Update Menu.js

--- src/Core/Install/Data/Cms/View/Settings/Email/Module/Menu.js
+++ src/Core/Install/Data/Cms/View/Settings/Email/Module/Menu.js
@@ -16,25 +16,54 @@ menu.init = () => {
     for(index=0; index < list.length; index++){
         let node = list[index];
         node.on('click', (event) => {
-            if(node.data('has', 'url')){
+            if(event.detail === 1 && node.hasClass('settings-email-add')){
                 const list = section.select('.nav-link');
                 list.removeClass('active');
                 node.addClass('active');
                 const body = section.select('.card-body');
                 body.addClass('display-none');
-                header('authorization', 'Bearer ' + user.token());
-                request(node.data('url'), null, (url, response) => {
-                    if(node.data('has', 'frontend-url')){
-                        request(node.data('frontend-url'), response);
+                const selected = section.select('.card-body-add');
+                if(selected){
+                    //select
+                    selected.removeClass('display-none');
+                } else {
+                    //load
+                    if(node.data('has', 'url')){
+                        header('authorization', 'Bearer ' + user.token());
+                        request(node.data('url'), null, (url, response) => {
+                            if(node.data('has', 'frontend-url')){
+                                request(node.data('frontend-url'), response);
+                            }
+                        });
                     }
-                });
+                    else if(node.data('has', 'frontend-url')){
+                        request(node.data('frontend-url'));
+                    }
+                }
+            } else {
+                if(node.data('has', 'url')){
+                    const list = section.select('.nav-link');
+                    list.removeClass('active');
+                    node.addClass('active');
+                    const body = section.select('.card-body');
+                    body.addClass('display-none');
+                    header('authorization', 'Bearer ' + user.token());
+                    request(node.data('url'), null, (url, response) => {
+                        if(node.data('has', 'frontend-url')){
+                            request(node.data('frontend-url'), response);
+                        }
+                    });
+                }
+                else if(node.data('has', 'frontend-url')){
+                    const list = section.select('.nav-link');
+                    list.removeClass('active');
+                    node.addClass('active');
+                    const body = section.select('.card-body');
+                    body.addClass('display-none');
+                    request(node.data('frontend-url'));
+                }
             }
-            else if(node.data('has', 'frontend-url')){
-                const list = section.select('.nav-link');
-                list.removeClass('active');
-                node.addClass('active');
-                request(node.data('frontend-url'));
-            }
+
         });
     }
 }
