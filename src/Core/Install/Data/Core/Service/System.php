ec7705fb81cad5fb1ac6a7df756f4e0486479558
Remco van der Velde <remco.pc@outlook.com>
2021-12-12 22:20:27 +0100

    Update System.php

--- src/Core/Install/Data/Core/Service/System.php
+++ src/Core/Install/Data/Core/Service/System.php
@@ -14,6 +14,32 @@ class System extends Main {
 
     public static function update_cms(App $object): Response
     {
+        dd($_SERVER['HTTP_AUTHORIZATION']);
+        $execute = '
+            funda admin task "
+                composer update && 
+                funda cache:clear &&
+                funda info all
+            "
+        ';
+        Core::execute($execute, $output);
+        if(array_key_exists(0, $output)){
+            $task = $output[0];
+            $dir = Dir::name($task);
+            $file = File::basename($task, '.task') . '.token';
+            $url = $dir . $file;
+            try {
+                $array = [];
+                $array['output'] = $object->config('project.dir.data')  . 'Output' . $object->config('ds') . File::basename($task);
+                $array['written'] = File::write($url, $token);
+                $array['token'] = $token;
+                return Core::object($array);
+            } catch (\R3m\Io\Exception\FileWriteException | \R3m\Io\Exception\ObjectException $e) {
+            }
+        }
+
+
+
         $record = [];
         $record['good'] = 'on you';
 
