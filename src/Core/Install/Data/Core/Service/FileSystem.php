1beb7e6f73b271eb7e620566665274e05a23bd6d
Remco van der Velde <remco.pc@outlook.com>
2021-12-12 23:32:32 +0100

    Update FileSystem.php

--- src/Core/Install/Data/Core/Service/FileSystem.php
+++ src/Core/Install/Data/Core/Service/FileSystem.php
@@ -29,7 +29,7 @@ class FileSystem {
      */
     public static function read(App $object): Response
     {
-        dd($object->request('node.url'));
+        dd($object->request());
         $url = htmlspecialchars_decode($object->request('node.url'), ENT_HTML5);
         if(File::exist($url)){
             $extension = File::extension($url);
