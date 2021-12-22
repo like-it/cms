992fbc7816cc6ce8edfede9b4083bb90829808d4
Remco van der Velde <remco.pc@outlook.com>
2021-12-22 23:12:22 +0100

    Update Admin.php

--- src/Core/Install/Data/Core/Service/Admin.php
+++ src/Core/Install/Data/Core/Service/Admin.php
@@ -165,8 +165,8 @@ class Admin extends Main
                             $output = [];
                             Dir::change($object->config('project.dir.root'));
                             Core::execute($task, $output);
-                            ob_end_clean();
                             File::write($dir_output . $basename, implode(PHP_EOL, $output));
+                            File::write($dir_output . 'test', implode(PHP_EOL, $output));
                         } else {
                             $content = 'No Administrator...' . PHP_EOL;
                             File::write($dir_output . $basename, $content);
