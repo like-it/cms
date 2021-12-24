d685fef5612d4f45f39fd1bc4f6632c9b9f5c6bf
Remco van der Velde <remco.pc@outlook.com>
2021-12-24 21:30:09 +0100

    Update Main.js

--- src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Module/Main.js
+++ src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Module/Main.js
@@ -1,4 +1,41 @@
 {R3M}
+import { getSectionByName } from "/Module/Section.js";
 ready(() => {
     console.log('ready logs & errors...');
+    const section = getSectionByName('main-content');
+    if(!section){
+        return;
+    }
+    const header = section.select('.card-header');
+    if(!header){
+        return;
+    }
+    const list = header.select('a');
+    if(!list){
+        return;
+    }
+    if(is.nodeList(list)){
+        let index;
+        for(index=0; index < list.length; index++){
+            let node = list[index];
+            node.on('click', (event) => {
+                if(node.data('url')){
+                    console.log('has url');
+                }
+                else if(node.data('frontend-url')){
+                    request(node.data('frontend-url'));
+                }
+            });
+        }
+    } else {
+        let node = list;
+        node.on('click', (event) => {
+            if(node.data('url')){
+                console.log('has url');
+            }
+            else if(node.data('frontend-url')){
+                request(node.data('frontend-url'));
+            }
+        });
+    }
 });
