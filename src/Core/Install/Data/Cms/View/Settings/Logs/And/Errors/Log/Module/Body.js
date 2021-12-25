ecffd71a230f73309f2c12833b064291ace7f805
Remco van der Velde <remco.pc@outlook.com>
2021-12-26 00:56:03 +0100

    Update Body.js

--- src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Log/Module/Body.js
+++ src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Log/Module/Body.js
@@ -4,6 +4,6 @@ import user from "/Module/User.js";
 
 ready(() => {
     console.log('body.js')
-    const request = "{{request()}}";
+    const request = "{{object(request(), 'json')}}";
     console.log(request);
 });
