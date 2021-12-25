b7f1dc7495343f0f8422459a6134f4c8315e487f
Remco van der Velde <remco.pc@outlook.com>
2021-12-26 00:37:55 +0100

    cms

---
+++ src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Log/Module/Body.js
@@ -0,0 +1,9 @@
+{R3M}
+import { getSectionByName } from "/Module/Section.js";
+import user from "/Module/User.js";
+
+ready(() => {
+    console.log('body.js')
+    const request = {{request()}};
+    console.log(request);
+});
---
+++ src/Core/Install/Data/Cms/View/Settings/Logs/And/Errors/Log/Section/Body.tpl
@@ -0,0 +1,5 @@
+{R3M}
+<div class="card-body h-100">
+    <h5 class="card-title">{{__('settings.logs_and_errors_section.main.body.title')}}</h5>
+    make table...
+</div>
