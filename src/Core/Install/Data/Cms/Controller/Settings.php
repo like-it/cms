20f642f89b46a70ec3078e45137ab7a0c66eb536
Remco van der Velde <remco.pc@outlook.com>
2021-12-27 14:46:23 +0100

    Update Settings.php

--- src/Core/Install/Data/Cms/Controller/Settings.php
+++ src/Core/Install/Data/Cms/Controller/Settings.php
@@ -45,7 +45,9 @@ class Settings extends View {
     public static function basic_site_main(App $object){
         d(file_exists("/Application/Host/Cms/Funda/World/View/Settings/Basic/Site/Main.tpl"));
         $output = [];
-        Core::execute('cat /Application/Host/Cms/Funda/World/View/Settings/Basic/Site/Main.tpl', $output);
+        $command = 'cat /Application/Host/Cms/Funda/World/View/Settings/Basic/Site/Main.tpl';
+        Core::execute($command, $output);
+        d($command);
         d($output);
         dd(File::exist("/Application/Host/Cms/Funda/World/View/Settings/Basic/Site/Main.tpl"));
         $name = Settings::name(__FUNCTION__, __CLASS__, '/');
