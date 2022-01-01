0fbc268a892e7fdbd35bd0b9ad4273945d05030a
Remco van der Velde <remco.pc@outlook.com>
2022-01-01 15:15:44 +0100

    cms

--- src/Core/Install/Data/Cms/Plugin/Function_response_view.php
+++ src/Core/Install/Data/Cms/Plugin/Function_response_view.php
@@ -1,25 +1,33 @@
 <?php
 
 use R3m\Io\Module\Core;
+use R3m\Io\Module\File;
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 
 function function_response_view(Parse $parse, Data $data, $options=[]){
     $object = $parse->object();
-
     $function = $object->config('project.dir.vendor') . 'r3m/framework/src/Plugin/Function_require.php';
     require_once $function;
-
-    $url = $options['prefix'] .
+    $list = [];
+    $list[] = $options['prefix'] .
         str_replace('-', $object->config('ds'), Core::ucfirst_sentence($options['submodule'], '-')) .
         $object->config('ds') .
         'Function' .
         $object->config('ds') .
         'Response.View.tpl'
     ;
+    $list[] = $options['prefix'] .
+        'Function' .
+        $object->config('ds') .
+        'Response.View.tpl'
+    ;
     $data_data = clone($data);
     $data_data->data(Core::object($options, Core::OBJECT_OBJECT));
-    $require = function_require($parse, $data_data, $url);
-
-    return $require;
+    foreach($list as $url){
+        if(File::exist($url)){
+            $require = function_require($parse, $data_data, $url);
+            return $require;
+        }
+    }
 }
