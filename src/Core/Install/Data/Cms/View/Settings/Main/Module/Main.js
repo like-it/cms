5486314fbc44f07139f3ef5f4f97b28f5a403181
Remco van der Velde <remco.pc@outlook.com>
2022-01-01 23:37:23 +0100

    cms

--- src/Core/Install/Data/Cms/View/Settings/Main/Module/Main.js
+++ src/Core/Install/Data/Cms/View/Settings/Main/Module/Main.js
@@ -1,7 +1,82 @@
 {R3M}
 import { getSectionByName } from "/Module/Section.js";
 
-ready(() => {
+let main = {};
+
+main.init = () => {
+    main.header();
+    main.select(0);
+    main.content();
+};
+
+main.select = (index) => {
+    const section = getSectionByName('main-content');
+    if(!section){
+        return;
+    }
+    const list = section.select('.nav-link');
+    if(is.nodeList(list)){
+        if(!is.empty(list[index])){
+            let node = list[index];
+            list.removeClass('active');
+            node.addClass('active');
+        }
+    } else {
+        let node = list;
+        if(node){
+            node.addClass('active');
+        }
+    }
+}
+
+main.header = () => {
+    const section = getSectionByName('main-content');
+    if(!section){
+        return;
+    }
+    const list = section.select('.nav-link');
+    let index;
+    if(is.nodeList(list)){
+        for(index=0; index < list.length; index++){
+            let node = list[index];
+            node.on('click', (event) => {
+                if(node.data('has', 'url')){
+                    list.removeClass('active');
+                    node.addClass('active');
+                    header('Authorization', 'Bearer ' + user.token());
+                    request(node.data('url'), null, (url, response) => {
+                        if(node.data('has', 'frontend-url')){
+                            request(node.data('frontend-url'), response);
+                        }
+                    });
+                }
+                else if(node.data('has', 'frontend-url')){
+                    request(node.data('frontend-url'));
+                }
+            });
+        }
+    } else if (list){
+        let node = list;
+        node.on('click', (event) => {
+            if(node.data('has', 'url')){
+                list.removeClass('active');
+                node.addClass('active');
+                header('Authorization', 'Bearer ' + user.token());
+                request(node.data('url'), null, (url, response) => {
+                    if(node.data('has', 'frontend-url')){
+                        request(node.data('frontend-url'), response);
+                    }
+                });
+            }
+            else if(node.data('has', 'frontend-url')){
+                request(node.data('frontend-url'));
+            }
+        });
+    }
+}
+
+
+main.content = () => {
     const section = getSectionByName('main-content');
     if(!section){
         return;
@@ -12,4 +87,8 @@ ready(() => {
         let node = list[index];
         node.trigger('click');
     }
+}
+
+ready(() => {
+    main.init();
 });
--- src/Core/Install/Data/Cms/View/Settings/Main/Module/Navigation.js
+++ src/Core/Install/Data/Cms/View/Settings/Main/Module/Navigation.js
@@ -2,59 +2,81 @@
 import user from "/Module/User.js";
 import { getSectionByName} from "/Module/Section.js";
 
-ready(() => {
-    const navigation = getSectionByName("navigation");
-    if(!navigation){
-        request("{{route.get(route.prefix() + '-navigation-main')}}");
-    }
+let navigation = {};
+
+navigation.init = () => {
+    const route = "{{route.get(route.prefix() + '-settings-main-main')}}";
+    window.history.pushState(route, route, route);
+    navigation.main();
+    navigation.section();
+    navigation.active();
+};
+
+navigation.active = () => {
     const section = getSectionByName("main-navigation");
     if(!section){
         console.warn('Cannot find section navigation...');
         return;
     }
-    const route = "{{route.get(route.prefix() + '-settings-main')}}";
-    window.history.pushState(route, route, route);
-    const list = section.select('a');
-    console.log(list);
-    let index;
-    for(index=0; index < list.length; index++){
-        let node = list[index];
-        node.on('click', (event) => {
-            if(node.hasClass('active')){
-                if(node.data('has', 'url')){
-                    header('Authorization', 'bearer ' + user.token());
-                    request(node.data('url'), null, (url, response) => {
-                        if(node.data('frontend-url')){
-                            request(node.data('frontend-url'), response);
-                        }
-                    });
-                }
-                else if(node.data('frontend-url')){
-                    console.log('clicked');
-                    request(node.data('frontend-url'));
-                }
-            } else {
-                list.removeClass('active');
-                node.addClass('active');
-                console.log(node);
-                console.log(node.data('has', 'url'));
-                if(node.data('has', 'url')){
-                    header('Authorization', 'bearer ' + user.token());
-                    request(node.data('url'), null, (url, response) => {
-                        if(node.data('frontend-url')){
-                            request(node.data('frontend-url'), response);
-                        }
-                    });
-                }
-                else if(node.data('frontend-url')){
-                    console.log('clicked');
-                    request(node.data('frontend-url'));
-                }
-            }
-        });
-    }
     const active = section.select('.active');
     if(active){
         active.trigger('click');
     }
+}
+
+navigation.main = () => {
+    const nav = getSectionByName("navigation");
+    if(!nav){
+        request("{{route.get(route.prefix() + '-navigation-main')}}");
+    }
+};
+
+navigation.section = () => {
+    const section = getSectionByName("main-navigation");
+    if(!section){
+        console.warn('Cannot find section navigation...');
+        return;
+    }
+    const list = section.select('a');
+    if(is.nodeList(list)){
+        let index;
+        for(index=0; index < list.length; index++){
+            let node = list[index];
+            node.on('click', (event) => {
+                list.removeClass('active');
+                node.addClass('active');
+                if(node.data('has', 'url')){
+                    header('Authorization', 'bearer ' + user.token());
+                    request(node.data('url'), null, (url, response) => {
+                        if(node.data('frontend-url')){
+                            request(node.data('frontend-url'), response);
+                        }
+                    });
+                }
+                else if(node.data('has', 'frontend-url')){
+                    request(node.data('frontend-url'));
+                }
+            });
+        }
+    } else if(list) {
+        let node = list;
+        node.on('click', (event) => {
+            node.addClass('active');
+            if(node.data('has', 'url')){
+                header('Authorization', 'bearer ' + user.token());
+                request(node.data('url'), null, (url, response) => {
+                    if(node.data('frontend-url')){
+                        request(node.data('frontend-url'), response);
+                    }
+                });
+            }
+            else if(node.data('has', 'frontend-url')){
+                request(node.data('frontend-url'));
+            }
+        });
+    }
+};
+
+ready(() => {
+    navigation.init();
 });
