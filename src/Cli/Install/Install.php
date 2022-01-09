2f46b38cd65198557293cc0f8d1639e0125eb729
Remco van der Velde <remco.pc@outlook.com>
2022-01-09 23:36:57 +0100

    Create Install.php

---
+++ src/Cli/Install/Install.php
@@ -0,0 +1,26 @@
+<?php
+namespace LikeIt\Cms\Cli\Install;
+
+$dir = __DIR__;
+$dir_vendor =
+    dirname($dir, 1) .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+if(!is_dir($dir_vendor)){
+    echo 'Please start bash in the CLI...';
+    return;
+}
+
+var_dump($dir_vendor);
+die;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+$config = new R3m\Io\Config(
+    [
+        'dir.vendor' => $dir_vendor
+    ]
+);
+$app = new R3m\Io\App($autoload, $config);
