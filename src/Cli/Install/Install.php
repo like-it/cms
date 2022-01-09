c0607c8cf8bc7dab8c77f5417ae58250bce487ad
Remco van der Velde <remco.pc@outlook.com>
2022-01-09 23:53:43 +0100

    Update Install.php

--- src/Cli/Install/Install.php
+++ src/Cli/Install/Install.php
@@ -1,20 +1,17 @@
 <?php
 namespace LikeIt\Cms\Cli\Install;
 
+use R3m\Io\Module\File;
+
 $dir = __DIR__;
 $dir_vendor =
     dirname($dir, 5) .
     DIRECTORY_SEPARATOR;
 
-var_dump($dir_vendor);
-die;
-
 if(!is_dir($dir_vendor)){
     echo 'Please start bash in the CLI...';
     return;
 }
-
-
 
 $autoload = $dir_vendor . 'autoload.php';
 $autoload = require $autoload;
@@ -24,3 +21,6 @@ $config = new R3m\Io\Config(
     ]
 );
 $app = new R3m\Io\App($autoload, $config);
+if(!File::exist($app->config('project.dir.data')) . 'Route.json'){
+    echo 'create route...';
+}
