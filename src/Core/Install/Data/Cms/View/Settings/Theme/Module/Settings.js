68d76ae409f1f32ee937450d63fc7037bf1c0ed0
Remco van der Velde <remco.pc@outlook.com>
2022-01-31 00:25:42 +0100

    Update Settings.js

--- src/Core/Install/Data/Cms/View/Settings/Theme/Module/Settings.js
+++ src/Core/Install/Data/Cms/View/Settings/Theme/Module/Settings.js
@@ -8,6 +8,43 @@ import { root } from "/Module/Web.js";
 
 let settings = {};
 
+settings.onDoubleClick = () => {
+    const section = getSectionByName('main-content');
+    if(!section){
+        return;
+    }
+    let list = section.select('.card-body-settings tr');
+    if(is.nodeList(list)){
+        let index;
+        for(index=0; index < list.length; index++){
+            let node = list[index];
+            node.on('dblclick', (event) => {
+                if(node.data('has', 'url')){
+                    header('Authorization', 'Bearer ' + user.token());
+                    request(node.data('url'), null, (url, response) => {
+                        request(node.data('frontend-url'), response, (frontendUrl, frontendResponse) => {
+
+                        });
+                    });
+                }
+            });
+        }
+    } else if(list) {
+        let node = list;
+        node.on('dblclick', (event) => {
+            if(node.data('has', 'url')){
+                header('Authorization', 'Bearer ' + user.token());
+                request(node.data('url'), null, (url, response) => {
+                    request(node.data('frontend-url'), response, (frontendUrl, frontendResponse) => {
+
+                    });
+                });
+            }
+        });
+
+    }
+}
+
 settings.body = () => {
     const section = getSectionByName('main-content');
     if(!section){
