c1a21ee657fc8dc7157a0926d1de89fa228919e0
Remco van der Velde <remco.pc@outlook.com>
2022-01-31 14:56:32 +0100

    Update Settings.php

--- src/Core/Install/Data/Cms/Controller/Settings.php
+++ src/Core/Install/Data/Cms/Controller/Settings.php
@@ -66,6 +66,76 @@ class Settings extends View {
         }
     }
 
+    public static function controllers_main(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        $name = explode('.', $name);
+        $name = implode('/', $name);
+        try {
+            if(App::contentType($object) == App::CONTENT_TYPE_HTML){
+                $url = Settings::locate($object, 'Main/Main');
+                $object->data('template.name', $name);
+                $object->data('template.dir', Settings::DIR);
+                $view = Settings::response($object, $url);
+            } else {
+                $url = Settings::locate($object, $name);
+                $view = Settings::response($object, $url);
+            }
+            return $view;
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
+    public static function controllers_main_body(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Settings::locate($object, $name);
+            return Settings::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
+    public static function controllers_settings_body(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Settings::locate($object, $name);
+            return Settings::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
+    public static function controllers_add_controller_body(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Settings::locate($object, $name);
+            return Settings::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
+    public static function controllers_edit_controller_body(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Settings::locate($object, $name);
+            return Settings::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
+    public static function controllers_view_body(App $object){
+        $name = Settings::name(__FUNCTION__, __CLASS__, '/');
+        try {
+            $url = Settings::locate($object, $name);
+            return Settings::response($object, $url);
+        } catch (Exception | LocateException | UrlEmptyException | UrlNotExistException $exception){
+            return $exception;
+        }
+    }
+
     public static function export_main(App $object){
         $name = Settings::name(__FUNCTION__, __CLASS__, '/');
         $name = explode('.', $name);
