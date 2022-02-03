f56736e3ca866ef9cee489e79167b26b94f5215f
Remco van der Velde <remco.pc@outlook.com>
2022-02-04 00:49:01 +0100

    Update Edit.js

--- src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Edit.js
+++ src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Edit.js
@@ -2,6 +2,8 @@
 import user from "/Module/User.js";
 import create from "/Module/Create.js";
 import menu from "/Module/Menu.js";
+import { version } from "/Module/Priya.js";
+import { root } from "/Module/Web.js";
 import { getSectionByName } from "/Module/Section.js";
 import { contains, replace } from "/Module/String.js";
 
