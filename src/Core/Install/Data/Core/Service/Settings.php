4e2e3aa153d39d37f62e4b263727ffbbbd693e6b
Remco van der Velde <remco.pc@outlook.com>
2022-02-03 01:17:31 +0100

    Update Settings.php

--- src/Core/Install/Data/Core/Service/Settings.php
+++ src/Core/Install/Data/Core/Service/Settings.php
@@ -127,7 +127,6 @@ class Settings extends Main {
      */
     public static function controllers_delete(App $object, $uuid): Response
     {
-        dd($object->request());
         $domain = Settings::domain_get($object);
         $url = Dir::name($domain->dir) . $object->config('dictionary.controller') . $object->config('ds') . $object->request('node.name');
 
