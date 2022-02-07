dce6667e02e982029210d3c44c68dd6c01df85c2
Remco van der Velde <remco.pc@outlook.com>
2022-02-07 19:40:05 +0100

    Update Source.js

--- src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Source.js
+++ src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Source.js
@@ -153,6 +153,7 @@ source.createLi = () => {
                         let sel = selected_pre.childNodes[ii];
                         if(position >= sel.innerText?.length){
                             position -= sel.innerText.length;
+                            console.log('position:', position);
                             node = sel;
                         } else {
                             if(sel.innerText?.length > 0){
