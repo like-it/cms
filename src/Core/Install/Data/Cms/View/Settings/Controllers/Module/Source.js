701eaaf3215c2a34b6fa5ce87014e14ef7f86bf3
Remco van der Velde <remco.pc@outlook.com>
2022-02-11 17:38:54 +0100

    cms

--- src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Source.js
+++ src/Core/Install/Data/Cms/View/Settings/Controllers/Module/Source.js
@@ -4,6 +4,35 @@ import { root } from "/Module/Web.js";
 import { getSectionByName } from "/Module/Section.js";
 import { contains, replace } from "/Module/String.js";
 
+(function(window){
+    window.htmlentities = {
+        /**
+         * Converts a string to its html characters completely.
+         *
+         * @param {String} str String with unescaped HTML characters
+         **/
+        encode : function(str) {
+            var buf = [];
+
+            for (var i=str.length-1;i>=0;i--) {
+                buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+            }
+
+            return buf.join('');
+        },
+        /**
+         * Converts an html characterSet into its original character.
+         *
+         * @param {String} str htmlSet entities
+         **/
+        decode : function(str) {
+            return str.replace(/&#(\d+);/g, function(match, dec) {
+                return String.fromCharCode(dec);
+            });
+        }
+    };
+})(window);
+
 let source = {};
 
 source.init = () => {
--- src/Core/Install/Data/Cms/View/Settings/Controllers/Section/Form/Component/Field/Content.tpl
+++ src/Core/Install/Data/Cms/View/Settings/Controllers/Section/Form/Component/Field/Content.tpl
@@ -5,7 +5,7 @@
     id="{{$pre.id}}"
     name="{{$pre.name}}"
 >
-{{$request.node.content}}
+{{$request.node.content|html.entity.encode}}
 </pre>
 {{script('module')}}
     {{require($prefix + $require.submodule + '/Module/Source.js')}}
