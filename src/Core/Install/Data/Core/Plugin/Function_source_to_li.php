f61aeeb4641d9997a1e71c947f2b7bffeb00c6c1
Remco van der Velde <remco.pc@outlook.com>
2022-03-03 18:25:48 +0100

    cms

--- src/Core/Install/Data/Core/Plugin/Function_source_to_li.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use R3m\Io\Module\Data;
-use R3m\Io\Module\Parse;
-
-function function_source_to_li(Parse $parse, Data $data, $read=''){
-    $object = $parse->object();
-    return \Host\Subdomain\Host\Extension\Service\Source::toLi($object, $read);
-}
