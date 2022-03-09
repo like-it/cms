9b97e471b77c2345f0676d52f55c661393d5dfb9
Remco van der Velde <remco.pc@outlook.com>
2022-03-09 23:23:28 +0100

    Update Menu.js

--- src/Core/Install/Data/Cms/View/Settings/Module/Menu.js
+++ src/Core/Install/Data/Cms/View/Settings/Module/Menu.js
@@ -116,6 +116,7 @@ menu.is_selected = (node, selected) => {
                         let message = "{{object(__($module + '.' + $submodule + '.' + 'dialog.error.domain.message'), 'json-data')}}";
                         message = message.replaceAll('&quot;', '"');
                         message = JSON.parse(message);
+                        console.log(message);
                         dialog.create({
                             title : "{{__($module + '.' + $submodule + '.' + 'dialog.error.domain.title')}}",
                             message : message,
