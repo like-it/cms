fb1f0d6d13e24f5924075cbae68c923b1f4d9e5c
Remco van der Velde <remco.pc@outlook.com>
2022-03-25 22:10:54 +0100

    Update Settings.php

--- src/Core/Install/Data/Core/Controller/Settings.php
+++ src/Core/Install/Data/Core/Controller/Settings.php
@@ -207,6 +207,7 @@ class Settings extends View {
     }
 
     public static function server_settings_command(App $object){
+        $object->logger()->debug('request' [ $object->request() ]);
         $url = $object->request('node.url');
         $nodeList = $object->request('nodeList');
         try {
