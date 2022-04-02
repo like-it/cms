34da42d881b311e161d873c78d974875202e6435
Remco van der Velde <remco.pc@outlook.com>
2022-04-02 23:39:00 +0200

    Update Settings.php

--- src/Core/Install/Data/Core/Controller/Settings.php
+++ src/Core/Install/Data/Core/Controller/Settings.php
@@ -237,6 +237,17 @@ class Settings extends View {
         }
     }
 
+    public static function server_settings_new(App $object)
+    {
+        $type = $object->request('type');
+        dd($type);
+        try {
+            return Service::server_settings_list($object);
+        } catch (Exception $exception){
+            return $exception;
+        }
+    }
+
     public static function theme_settings(App $object): Response
     {
         return Service::theme_list($object);
