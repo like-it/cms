46924457ca89374607fd23c5c9b269e3410406c7
Remco van der Velde <remco.pc@outlook.com>
2022-04-10 23:29:14 +0200

    Update Settings.php

--- src/Core/Install/Data/Core/Service/Settings.php
+++ src/Core/Install/Data/Core/Service/Settings.php
@@ -1509,7 +1509,6 @@ class Settings extends Main {
             for($i=count($explode); $i >= 2; $i--){
                 $dir_example = implode($object->config('ds'), $explode);
                 array_pop($explode);
-                dd($dir_example);
                 if(
                     File::is_link($dir_example) &&
                     $url !== $dir_example
