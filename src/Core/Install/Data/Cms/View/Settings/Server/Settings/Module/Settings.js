cfa03ff1007419ec19dec935b78a8d43ed90b54f
Remco van der Velde <remco.pc@outlook.com>
2022-04-20 23:10:06 +0200

    Update Settings.js

--- src/Core/Install/Data/Cms/View/Settings/Server/Settings/Module/Settings.js
+++ src/Core/Install/Data/Cms/View/Settings/Server/Settings/Module/Settings.js
@@ -910,13 +910,10 @@ settings.node.list.upload = ({node, section, target}) => {
         if(!form){
             return;
         }
-        const button = form.select('input[type="submit"]');
-        if(button){
-            button.on('click', (event) => {
-                dialog_create.remove();
-                menu.dispatch(section, target);
-            });
-        }
+        form.on('submit', (event) => {
+            dialog_create.remove();
+            menu.dispatch(section, target);
+        });
         const input = form.select('input[name="node.directory"]');
         if(input){
             input.focus();
