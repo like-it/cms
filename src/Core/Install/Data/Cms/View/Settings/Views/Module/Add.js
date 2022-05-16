50fc70d4048ab9638ab31978d4a633063b6daad4
Remco van der Velde <remco.pc@outlook.com>
2022-05-16 14:20:44 +0200

    Update Add.js

--- src/Core/Install/Data/Cms/View/Settings/Views/Module/Add.js
+++ src/Core/Install/Data/Cms/View/Settings/Views/Module/Add.js
@@ -2,8 +2,68 @@
 import user from "/Module/User.js";
 import menu from "/Module/Menu.js";
 import { getSectionByName } from "/Module/Section.js";
+import { uuid } from "/Module/Web.js";
 
 let add = {};
+
+add.title = () => {
+    const section = getSectionByName('main-content');
+    if(!section){
+        return;
+    }
+    const attribute = uuid();
+    const is_link = section.select('.' + "{{$module}}" + '-' + "{{$submodule}}" + '-' + "{{$command}}" + '-' + attribute);
+    if(is_link){
+        const nav = section.select('.nav');
+        const active = nav.select('.active');
+        if(active){
+            active.removeClass('active');
+        }
+        is_link.addClass('active');
+        add.body();
+        return;
+    }
+    const nav = section.select('.nav');
+    const li = create('li', 'nav-item');
+    const a = create('a', 'nav-link ' + "{{$module}}" + '-' + "{{$submodule}}" + '-' + "{{$command}}" + '-' + attribute);
+    a.data('frontend-url', "");
+    a.data('url', "");
+    a.html("<span class='title'>Add file</span><i class=\"fas fa-window-close\"></i>");
+    li.append(a);
+    nav.append(li);
+    a.on('click', (event) => {
+        if(event.detail === 1){
+            const ul = li.parentNode;
+            const active = ul.select('.active');
+            if(active){
+                active.removeClass('active');
+            }
+            a.addClass('active');
+            add.body('show');
+        } else {
+            //reload from data url & frontend url
+        }
+    });
+    const i = a.select('.fa-window-close');
+    if(i){
+        i.on('click', (event) => {
+            event.stopPropagation();
+            li.remove();
+            add.body('remove');
+            const menuItem = section.select('.'+ "{{$module}}" + '-' + "{{$submodule}}" + '-' + 'settings');
+            if(menuItem){
+                menuItem.trigger('click');
+            }
+        });
+    }
+    const ul = li.parentNode;
+    const active = ul.select('.active');
+    if(active){
+        active.removeClass('active');
+    }
+    a.addClass('active');
+}
+
 
 add.body = () => {
     const section = getSectionByName('main-content');
@@ -138,6 +198,7 @@ add.focus = () => {
 
 add.init = () => {
     add.body();
+    add.title();
     add.onChange();
     add.form({
         select : [
