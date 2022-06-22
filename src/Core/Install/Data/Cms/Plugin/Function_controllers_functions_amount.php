d0b312668beccb37ee665bc2aa4758efec5f3130
Remco van der Velde <remco.pc@outlook.com>
2022-06-22 22:26:59 +0200

    Update Function_controllers_functions_amount.php

--- src/Core/Install/Data/Cms/Plugin/Function_controllers_functions_amount.php
+++ src/Core/Install/Data/Cms/Plugin/Function_controllers_functions_amount.php
@@ -6,7 +6,6 @@ use R3m\Io\Module\Parse;
 function function_controllers_functions_amount(Parse $parse, Data $data, $node = null){
     $object = $parse->object();
     $route = $object->route();
-    dd($node);
     $controller = str_replace('/', '.', $node->url);
     if(File::extension($node->url) === 'php'){
         $controller = File::basename($controller, $object->config('extension.php'));
@@ -14,23 +13,5 @@ function function_controllers_functions_amount(Parse $parse, Data $data, $node =
     } else {
         throw new Exception('Cannot process files other than .php');
     }
-    $count = 0;
-    if($route){
-        foreach($route->data() as $key => $record){
-            if(property_exists($record, 'controller')){
-                if(stristr($record->controller, $controller) !== false){
-                    $reverse = strrev($record->controller);
-                    $reverse = explode('.', $reverse, 2);
-                    $test = false;
-                    if(array_key_exists(1, $reverse)){
-                        $test = strrev($reverse[1]);
-                    }
-                    if($test === $controller){
-                        $count++;
-                    }
-                }
-            }
-        }
-    }
-    return $count;
+    dd($controller);
 }
