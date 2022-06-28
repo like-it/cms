bdc0bd218053600ddf0a3ba2a53f43ffce6e7cd5
Remco van der Velde <remco.pc@outlook.com>
2022-06-29 00:10:21 +0200

    Update System.php

--- src/Core/Install/Data/Core/Service/System.php
+++ src/Core/Install/Data/Core/Service/System.php
@@ -206,7 +206,7 @@ class System extends Main {
                         $state->write($url_state);
                         File::chown($url_state, 'www-data', 'www-data');
                         $command = 'runuser --user www-data -- php /Application/Bin/R3m.php parse compile ' . $optimization->template . ' ' . $optimization->data . ' state ' . $url_state;
-                        $command = 'runuser --user www-data -- php -f /Application/Bin/R3m.php info all > test.info';
+                        $command = '/sbin/runuser --user www-data -- php -f /Application/Bin/R3m.php info all > test.info';
                         //$command = 'php -f /Application/Bin/R3m.php info all > test.info';
                         //$command = 'runuser --user www-data -- composer install';
                         $output = [];
