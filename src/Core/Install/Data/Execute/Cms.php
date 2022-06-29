2f0c789eb69017c6d51407d19853b1831970a39f
Remco van der Velde <remco.pc@outlook.com>
2022-06-29 23:49:56 +0200

    Update Cms.php

--- src/Core/Install/Data/Execute/Cms.php
+++ src/Core/Install/Data/Execute/Cms.php
@@ -1,6 +1,8 @@
 <?php
 namespace Execute;
 
+use R3m\Io\Config;
+use R3m\Io\Module\Dir;
 use R3m\Io\Module\Host;
 
 trait Cms {
@@ -37,8 +39,55 @@ trait Cms {
 
 
     public function controller($title=''){
-        dd($this->object()->config('project.dir'));
-        d($this->object()->get('host'));
+        $object = $this->object();
+        $host = $object->get('host');
+        if(
+            property_exists($host, 'host') &&
+            property_exists($host, 'extension')
+        ){
+            if(empty($host->subdomain)){
+                $object->config('host.dir.root',
+                    $object->config('project.dir.host') .
+                    ucfirst($host->host) .
+                    $object->config('ds') .
+                    ucfirst($host->extension) .
+                    $object->config('ds')
+                );
+            } else {
+                $object->config('host.dir.root',
+                    $object->config('project.dir.host') .
+                    ucfirst($host->subdomain) .
+                    $object->config('ds') .
+                    ucfirst($host->host) .
+                    $object->config('ds') .
+                    ucfirst($host->extension) .
+                    $object->config('ds')
+                );
+            }
+            $object->config('host.dir.data',
+                $object->config('host.dir.root') .
+                $object->config(Config::DICTIONARY . '.' . Config::DATA) .
+                $object->config('ds')
+            );
+            $object->config('host.dir.cache',
+                Dir::name($object->config('framework.dir.cache'), 2) .
+                $object->config(Config::DICTIONARY . '.' . Config::HOST) .
+                $object->config('ds')
+            );
+            $object->config('host.dir.public',
+                $object->config('host.dir.root') .
+                $object->config(Config::DICTIONARY . '.' . Config::PUBLIC) .
+                $object->config('ds'));
+            $object->config('host.dir.source',
+                $object->config('host.dir.root') .
+                $object->config(Config::DICTIONARY . '.' . Config::SOURCE) .
+                $object->config('ds'));
+            $object->config('host.dir.view',
+                $object->config('host.dir.root') .
+                $object->config(Config::DICTIONARY . '.' . Config::VIEW) .
+                $object->config('ds')
+            );
+        }
         dd($title);
     }
 }
