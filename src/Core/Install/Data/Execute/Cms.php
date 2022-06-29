424471020d3eec60c6db80df74ee64d3ecc6208e
Remco van der Velde <remco.pc@outlook.com>
2022-06-29 23:23:46 +0200

    Update Cms.php

--- src/Core/Install/Data/Execute/Cms.php
+++ src/Core/Install/Data/Execute/Cms.php
@@ -1,10 +1,38 @@
 <?php
 namespace Execute;
 
+use R3m\Io\Module\Host;
+
 trait Cms {
 
     public function host(){
-        dd($this->object());
+        $host_url = $this->object()->config('project.dir.data') . 'Host' . $this->object()->config('extension.json');
+        $is_cms = false;
+        $hosts = $this->object()->data_read($host_url);
+        if($hosts){
+            foreach($hosts->get() as $uuid => $host){
+                if(
+                    property_exists($host, 'subdomain') &&
+                    $host->subdomain === 'cms'
+                ){
+                    $is_cms = true;
+                    break;
+                }
+            }
+        }
+        if(
+            $is_cms &&
+            !empty($host) &&
+            property_exists($host, 'subdomain') &&
+            property_exists($host, 'host') &&
+            property_exists($host, 'extension') &&
+            property_exists($host, 'name')
+        ){
+
+            $host->scheme = Host::SCHEME_HTTPS;
+            $host->url = $host->scheme . '://' . $host->name . '/';
+            $this->object()->set('host', $host);;
+        }
     }
 
 
